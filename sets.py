d465c6ab35562232fcae87fe1e15d8220dcd68b8
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2026-08-21 18:48:35 +0300

    add set file

---
+++ sets.py
@@ -0,0 +1,20 @@
+# numbers = {1,5,4,8,7,69,5,8}
+# print(numbers)
+# print(type(numbers))
+# names = {'ahmed' , 'taha' , 'taha' , 'ali'}
+# print(names)
+
+# names=set()
+# print(type(names))
+
+a = {1,2,3}
+b = {3,4,5}
+
+# print(a | b) #a unio b 
+# print(a & b ) # intersection
+# print( a -b) 
+
+a.add(24)
+print(a)
+a.discard(24)
+print(a)
